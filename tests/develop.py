58be4cb4960bf1e15328ed99d030657919a47b2f
Andre Marcio <andre.marcio.ti@gmail.com>
2026-02-19 19:04:34 -0300

    solving challenge

---
+++ tests/develop.py
@@ -0,0 +1,5 @@
+sorted_variables = {
+    "mutable": [meus_filmes_favoritos, marcas],
+    "immutable": [numero_da_sorte, pi, um_e_um_numero_primo, nome, informacoes_do_perfil, colecao_de_moedas]
+
+}
