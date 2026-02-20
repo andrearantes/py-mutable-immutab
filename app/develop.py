59b13ae979d2fb066b4d7bed32029275280e0347
Andre Marcio <andre.marcio.ti@gmail.com>
2026-02-20 10:25:08 -0300

    verify task

--- app/develop.py
+++ app/develop.py
@@ -1,3 +1,5 @@
+from app.main import my_favourite_films, collection_of_coins, marks, lucky_number, pi, one_is_a_prime_number, name, profile_info
+
 sorted_variables = {
     "mutable": [my_favourite_films, marks, collection_of_coins],
     "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
